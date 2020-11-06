2622f6a28e7bedb6caca98cba77938f33e601571
Lucas Sousa <god-lucas@hotmail.com>
2020-11-05 22:17:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import express from 'express';
 import socketIO from 'socket.io';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-/*
-app.get('/', function(req, res){
-    res.sendFile('./client_index.html');
-});
-*/
-
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'client_index.html')));
-
 const io = socketIO(app.listen(PORT, () => console.log(`listenning on port ${PORT}...`)), { pingInterval: 1500 });
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const gameWidth = 10;
 const gameHeight = 10;
